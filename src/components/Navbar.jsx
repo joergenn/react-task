8878b5937b19230ce76a100d05e34707881984c7
joergenn <andrian1993a@gmail.com>
2023-10-12 15:39:58 +0300

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,6 @@ function Navbar() {
     <div className="navbar">
         <div className="navbar-links">
           <Link className="link_to" to="/login">Login</Link>
-          <Link className="link_to" to="/posts">Posts</Link>
-          <Link className="link_to" to="/news">News</Link>
-          <Link className="link_to" to="/profile">Profile</Link>
         </div>
       </div>
     );
